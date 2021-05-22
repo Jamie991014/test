a230eba812d964a0704222e8f28df7ccd5dcaecd
zhishangw <zhishangw@student.unimelb.edu.au>
2021-05-23 06:20:58 +1000

    one hour

--- client/src/components/OrderBrief.js
+++ client/src/components/OrderBrief.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Modal, Button, Tooltip, OverlayTrigger } from 'react-bootstrap'; 
-import { Card, notification } from 'antd';
+import { ImputNumber, Card, notification, message } from 'antd';
 import { EyeOutlined, EditOutlined, CheckOutlined } from '@ant-design/icons';
 
+
 import CountUp from './CountUp.js';
+import axios from "../commons/axios";
 
 const { Meta } = Card;
 
@@ -47,6 +49,8 @@ export default class   extends React.Component {
     constructor(props){
         super();
         this.state = {
+            menu: [],
+            order: [],
             modalVisible: false,
             editModalVisibale: false,
             modalBody: <> </>,
@@ -60,6 +64,13 @@ export default class   extends React.Component {
     handleEditClose = () => this.setState({editModalVisible: false});
     handleEditShow = () => this.setState({editModalVisible: true});
 
+    onChange = (index, event) => {
+        let newArray = [...this.state.order];
+        newArray[index] = event;
+        this.setState({order: newArray});
+
+    }
+
     tick(){
         let now = new Date().getTime()
         let upd = Date.parse(this.props.order.updatedAt)
@@ -67,6 +78,9 @@ export default class   extends React.Component {
     }
 
     componentDidMount(){
+        axios.get('/snack').then(response => {
+            this.setState({ menu: response.data.snacks})
+        })
         this.timerID = setInterval(() => this.tick(), 1000); // updates this DOM every second
     }
 
@@ -80,6 +94,110 @@ export default class   extends React.Component {
 
     handleEditOrder = () => {
         console.log(this.state.diff)
+        if (this.props.order.status === "outstanding" && this.state.diff <= 10){
+            this.setState({editModalVisible: true});
+        }
+        if(this.props.order.status === "fulfilled") {
+            notification.open({
+                message: 'Order is ready to be collected!',
+                description: 'You cannot make any changes to a fulfilled order. You can rate your experience after picking up the snacks',
+                duration: 3
+
+            });         
+        }else if (this.props.order.status === "outstanding" && this.state.diff > 10){
+            notification.open({
+                message: 'Order is being processed!',
+                description: 'You can only update your order within 10 minutes after placing the order.',
+                duration: 3
+            });
+        }else{
+            console.log(props.order)
+            this.setState({editModalVisible: true});
+
+        }
+    }
+
+    renderEditModalContent = () => {
+        if(this.props.order.status === "outstanding"){
+            return(
+                <>
+                <Modal.Header closeButton>
+                    <Modal.Title>Menu</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                {this.state.menu.map((snack, index) =>(
+                        <Card cover={<img alt="example" src={snack.image} />}style={{marginBottom:"2vh"}}size={"small"} key={snack._id}>
+                            <Meta
+                                title={snack.name + "    " + snack.price}
+                            />
+                            <Divider style={{borderWidth:5, borderColor: '#593e34' }} plain>
+                            </Divider>
+                            <Meta
+                                description={snack.detail}
+                            />
+                            <InputNumber key ={snack._id} min={0} defaultValue={0} style ={{marginLeft:"80%"}} onChange={e => this.onChange(index, e)} />
+                                
+                        </Card>
+                    ))}
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="dark" onClick={() => this.onOrderSubmit()}>
+                        Submit
+                    </Button>
+                </Modal.Footer>
+            </>
+            )
+
+        }else{
+            return(
+                <>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{"OrderId: " + this.props.order._id}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>Vendor: {this.props.order.vendor._id}</p>
+                        <p>Snacks: {this.props.order.snacks.map((snack) => <li key={snack.name}>{snack.name} - qty: {snack.qty}</li>)}</p>
+                        <p>comment and rating</p>
+                    </Modal.Body>
+                </>
+            )
+        }
+        
+    }
+
+    onOrderSubmit = () => {        
+            var submitOrder = []       
+            for (var i = 0; i < this.state.order.length; i++){
+                if(Number.isFinite(this.state.order[i])){
+                    submitOrder.push({
+                        "name": this.state.menu[i].name,
+                        "qty":this.state.order[i]
+                    })
+                }
+            }
+            if (submitOrder.length ===0){
+                this.setState({editModalVisible: false });
+                message.error("You need to enter more than one snack!")
+
+            }else{
+                //console.log(this.props.order.customer._id)
+                axios.post('/order/'+this.props.order._id + '/update',{
+                    //customer:this.props.order.customer.id,
+                    //vendor: this.props.order.vendor.id, //will be changed in the future
+                    status: "outstanding",
+                    snacks: submitOrder
+                }).then(response =>{
+                    if(response.data.success){
+                        message.success("Order has been placed!")
+                        this.setState({editModalVisible: false });
+                    }else{
+                        message.error("Order placing errored!")
+                    }
+                })
+            }
+            
+        
+        
     }
 
     render() {
@@ -93,6 +211,9 @@ export default class   extends React.Component {
                         <p>Vendor: {this.props.order.vendor._id}</p>
                         <p>Snacks: {this.props.order.snacks.map((snack) => <li key={snack.name}>{snack.name} - qty: {snack.qty}</li>)}</p>
                     </Modal.Body>
+                </Modal>
+                <Modal show={this.state.editModatVisible} onHide={() => this.handleEditClose()}>
+                   {this,this.renderEditModalContent()}
                 </Modal>
                 <Card style={{margin: "10px"}} 
                 actions={[<EyeOutlined onClick = {() => this.handleShow()} />, 
